7ba0339db5a5d6e7177380bcc7fddae1aab9c472
MackRoe <elaine.music@students.makeschool.com>
2020-02-03 15:08:09 -0800

    server.js renders handlebars templated home/main

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// Middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine','handlebars');
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => res.render('home'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
