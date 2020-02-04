5056d5a4a481c520d9a33e8ddf9072c250fa8084
MackRoe <elaine.music@students.makeschool.com>
2020-02-03 17:05:27 -0800

    add New Post button to navbar. modify New Post form

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+// Setup
 const express = require('express')
 const app = express()
 const port = 3000
 
+/* Mongoose Connection */
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb://localhost/reddit-db",
+  { useNewUrlParser: true }
+);
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
+mongoose.set("debug", true);
 
 // Middleware
 const exphbs = require('express-handlebars');
