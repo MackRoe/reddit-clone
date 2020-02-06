70e57b539ea8bf809255030fc4ef5c0e1641e4ca
MackRoe <elaine.music@students.makeschool.com>
2020-02-05 17:19:40 -0800

    user can submit and save posts

--- server.js
+++ server.js
@@ -2,17 +2,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+
+
 
 /* Mongoose Connection */
 const mongoose = require("mongoose");
 
+
 mongoose.Promise = global.Promise;
+// mongoose.connect(
+//   url,
+//   {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+// )
 mongoose.connect(
   "mongodb://localhost/reddit-db",
   { useNewUrlParser: true }
 );
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
 mongoose.set("debug", true);
+
+// Set db
+require('./data/reddit-db');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+require('./controllers/posts.js')(app);
 
 // Middleware
 const exphbs = require('express-handlebars');
