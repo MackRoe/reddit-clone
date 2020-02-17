8afbb33205cf68ebb0be0ca1eb7cf3d2ab4fab80
MackRoe <elaine.music@students.makeschool.com>
2020-02-17 15:58:13 -0800

    User can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ app.post("/sign-up", (req, res) => {
   const user = new User(req.body);
 
   user.save().then((user) => {
-      var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+      let token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
       res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
       res.redirect('/');
     })
@@ -26,6 +26,10 @@ app.post("/sign-up", (req, res) => {
     res.clearCookie('nToken');
     res.redirect('/');
     });
+    // log in FORM
+    app.get("/login", (req, res) => {
+      res.render("login");
+  });
     // LOGIN
 app.post("/login", (req, res) => {
   const username = req.body.username;
